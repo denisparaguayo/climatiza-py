972b7c60ff42bae808418c27bdc2d567ac0838da
Denis Cantero <denis.cantero1987@gmail.com>
2024-03-30 21:40:21 -0400

    todo

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
+// Coloque los datos globales en este archivo.
+// Puede importar estos datos desde cualquier lugar de su sitio utilizando la palabra clave 'import`.
 
-export const SITE_TITLE = 'Astro Blog';
+export const SITE_TITLE = 'ClimatizaPy';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
---
+++ src/content/blog/locura.md
@@ -0,0 +1,11 @@
+---
+title: locura
+description: nadaaa
+pubDate: 2024-03-31T00:30:33.281Z
+heroImage: /hero.png
+type: default
+---
+
+# nada
+
+saaa sa sa ya cusa
