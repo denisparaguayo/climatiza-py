675afa53bb605042b86ac378cb2d49aaa410661f
Denis Cantero <denis.cantero1987@gmail.com>
2024-04-05 11:02:27 -0400

    adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from '@astrojs/tailwind';
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://climatizapy.com',
-	integrations: [mdx(), sitemap(), tailwind()],
-	output: 'hybrid',
+  site: 'https://climatizapy.com',
+  integrations: [mdx(), sitemap(), tailwind()],
+  output: "server",
+  adapter: netlify()
 });
