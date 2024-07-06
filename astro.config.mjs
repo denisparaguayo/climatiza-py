0c808b003f0183fa0a9fed1534e0a641a1543e31
Denis Cantero <denis.cantero1987@gmail.com>
2024-07-06 02:17:54 -0400

    site map

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+
 import tailwind from '@astrojs/tailwind';
 import netlify from '@astrojs/netlify';
 
@@ -9,14 +9,7 @@ import robotsTxt from 'astro-robots-txt';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://climatizapy.com',
-	integrations: [
-		mdx(),
-		sitemap(),
-		tailwind(),
-		robotsTxt({
-			sitemap: false,
-		}),
-	],
+	integrations: [mdx(), tailwind(), robotsTxt()],
 	output: 'server',
 	adapter: netlify(),
 });
